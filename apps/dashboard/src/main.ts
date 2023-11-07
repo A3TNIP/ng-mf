70c8a3442f67f3c2cbdc8d58872d912c03291304
Aaju Maharjan <aaju.maharjan@islingtoncollege.edu.np>
2023-11-07 15:07:43 +0545

    containerized micro-frontend

--- apps/dashboard/src/main.ts
+++ apps/dashboard/src/main.ts
@@ -1,6 +1,16 @@
 import { setRemoteDefinitions } from '@nx/angular/mf';
+import {environment} from "./environments/environment";
 
-fetch('/assets/module-federation.manifest.json')
+let manifestFile = '';
+switch(environment.isLocal) {
+  case true:
+    manifestFile = '/assets/module-federation.manifest.local.json';
+    break;
+  default:
+    manifestFile = '/assets/module-federation.manifest.json';
+}
+
+fetch(manifestFile)
   .then((res) => res.json())
   .then((definitions) => setRemoteDefinitions(definitions))
   .then(() => import('./bootstrap').catch((err) => console.error(err)));
